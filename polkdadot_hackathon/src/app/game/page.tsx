5b5ec246384c822c63c4bf3508110cac0a445648
Stone Liu <sliu@spillcenter.com>
2024-07-28 10:50:22 -0400

    transition text

--- polkdadot_hackathon/src/app/game/page.tsx
+++ polkdadot_hackathon/src/app/game/page.tsx
@@ -1,6 +1,23 @@
-import GuessGame from "@/components/Game"
+"use client"
+import React, { useState, useEffect } from "react"
+import TransitionText from "@/components/Game"
 import HorrorGame from "@/components/horror/HorrorGame"
 
 export default function Game() {
-  return <HorrorGame />
+  const [showHorrorGame, setShowHorrorGame] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowHorrorGame(true)
+    }, 8000)
+
+    return () => clearTimeout(timer) // Cleanup the timer on component unmount
+  }, [])
+
+  return (
+    <div>
+      <div className="flex justify-center">{!showHorrorGame && <TransitionText time={2000} />}</div>
+      {showHorrorGame && <HorrorGame />}
+    </div>
+  )
 }
--- polkdadot_hackathon/src/components/Game.tsx
+++ polkdadot_hackathon/src/components/Game.tsx
@@ -1,38 +1,25 @@
-import {
-  Accordion,
-  AccordionItem,
-  Button,
-  Card,
-  CardBody,
-} from "@nextui-org/react"
+"use client"
+import React from 'react';
+import TextTransition, { presets } from 'react-text-transition';
 
-const defaultContent =  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+const TEXTS = ['Get Ready...', 'For...', 'The...', 'Greatest Game of All Time!'];
 
+const TransitionText = ({ time } : any ) => {
+  const [index, setIndex] = React.useState(0);
 
-const components = [
-  <Button color="primary">Button</Button>,
-  <Card>
-    <CardBody>
-      <p>Card Content</p>
-    </CardBody>
-  </Card>,
-  // <Button color="secondary">Button</Button>,
-  // <Button color="danger">Button</Button>,
-  // <Button color="success">Button</Button>,
-]
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      time, 
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
 
-// Utility function to get 3 random components
-const getRandomComponents = () => {
-  const shuffled = components.sort(() => 0.5 - Math.random())
-  return shuffled.slice(0, 3)
-}
-
-export default function GuessGame() {
   return (
-    <div>
-      {getRandomComponents().map((component) => {
-        return <div> {component} </div>
-      })}
-    </div>
-  )
-}
+    <h1 className='text-9xl'>
+      <TextTransition springConfig={presets.wobbly}>{TEXTS[index % TEXTS.length]}</TextTransition>
+    </h1>
+  );
+};
+
+export default TransitionText
